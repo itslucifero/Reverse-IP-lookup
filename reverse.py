430bc179332e57c1aa168786138f28b4f307180b
its Yahia <85070360+hrackedz@users.noreply.github.com>
2022-04-09 06:16:12 +0200

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,5 +1,6 @@
 # i used this modules if u got better ones please add :p
 #telegram me : https://t.me/kickflap
+from importlib.resources import contents
 import os
 import sys
 import json
@@ -15,27 +16,7 @@ if int(py_version[0]) == 2:
 if int(py_version[0]) == 3:
    from queue import Queue
 
-# My Used colors :p
-if sys.platform in ["linux","linux2"]:
-   purple = '\033[95m'
-   blue = '\033[94m'
-   cyan = '\033[96m'
-   green = '\033[92m'
-   yellow = '\033[93m'
-   red = '\033[91m'
-   end = '\033[0m'
-   bold = '\033[1m'
-   u = '\033[4m'
-else:
-   purple = ''
-   blue = ''
-   cyan = ''
-   green = ''
-   yellow = ''
-   red = ''
-   end = ''
-   bold = ''
-   u = ''
+
 
 #clearing the interpreter in both systems Linux and WINDOWS
 if sys.platform == 'win32':
@@ -43,27 +24,27 @@ if sys.platform == 'win32':
 else:
    os.system('clear')
 
-# Our class for the main reverse job
+#  takhreb nikek barkana
+
+
 class Reverse_Ip:
    def __init__(self):
-      self.url = 'https://reverseip-tools.com/api?q='
+      self.url = 'https://sonar.omnisint.io/reverse/'
       self.banner()
       self.save = 'hrackedz.txt'
    def reverse(self, domain):
         try:
             req = requests.get(self.url+domain)
+            
             if req.status_code == 200:
-                jumlah = len(req.json()['result'])
-                if jumlah > 10  and  jumlah < 100:
-                    color = yellow
-                elif jumlah < 10:
-                    color = red
-                else:
-                    color = green
-                print ("{} >> ({} domains)".format(color+domain, str(jumlah)))
+                
+                
+                bb = requests.get(self.url+domain).text
+                
+                print (f"{domain} >> ({len(bb)} domains)")
                 res = req.json()
-                if res['result']:
-                    domains = '\n'.join(res['result'])
+                if res:
+                    domains = '\n'.join(res)
                     open(self.save, 'a').write(domains+'\n')
         except Exception as v:
             print(v)
@@ -87,20 +68,19 @@ class Reverse_Ip:
  ░  ░  ░   ░           ░  ░░ ░      ░  ░      ░  ░   ░      ░ ░    
                            ░                       ░      ░        
                         Telegram : https://t.me/kickflap
-                        website : https://hrackedz.com
-                        Reverse IP tool :
+                        Discord : https://discord.gg/DmrrQkuUn6
+                        Reverse IP lookup V2.0 :
                         Grab massive domains from ips
                         you know where to use them :3    
                             
                              """  
 
                              
-      co = random.choice([red,purple,blue])
-      ct = random.choice([green,cyan,yellow])
-      banner = b.replace('◢◤',co+'◢◤').replace('R',ct+'R').replace('B',ct+'B')
-      print(banner)
+     
+      
+      print(b)
    def input(self, q):
-      #input based on python version
+      #input based on python version 2 or 3 ya boubgra
       if int(py_version[0]) == 3:
            return input(str(q))
       elif int(py_version[0]) == 2:
@@ -109,15 +89,15 @@ class Reverse_Ip:
       jobs = Queue()
       thread = 10
       try:
-          filename = self.input(yellow+'Give path to Domains/Ip List -> '+green)
-          list = open(filename, 'r').read().splitlines()
+          hehe = self.input('Give path to Domains/Ip List -> ')
+          list = open(hehe, 'r').read().splitlines()
       except IOError:
-           exit(red+"File Not Found!\n")
+           exit("File Not Found!\n")
       except FileNotFoundError:
-           exit(red+"File Not Found!\n")
-      print (red+'\nBetter using no more than (10) threads \nOr the tool will crash!')
-      thread = int(self.input(yellow+'Thread -> '+green))
-      print(blue+'\nResult will be saved in: hrackedz.txt')
+           exit("File Not Found!\n")
+      print ('\nBetter using no more than (10) threads \nOr the tool will crash!')
+      thread = int(self.input('Thread -> '))
+      print('\nResult will be saved in: hrackedz.txt')
       for dom in list:
          if dom:
              jobs.put(dom)
